a9945a6e00e633e623f870c7d99e223e75fd0fc9
AnnPTit <anptph27230@fpt.edu.vn>
2023-07-08 01:09:49 +0700

    Add_Permision_API

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.example.demo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
--- src/main/java/com/example/demo/controller/PositionController.java
+++ src/main/java/com/example/demo/controller/PositionController.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Deposit;
 import com.example.demo.entity.Position;
 import com.example.demo.service.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
