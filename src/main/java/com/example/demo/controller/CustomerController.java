d1ab9ceb52c34a054365a839f475dc4a6cc80e1a
tuann1 <tuannvph27234@fpt.edu.vn>
2023-07-12 20:55:20 +0700

    Validate_Customer

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -2,9 +2,15 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Customer;
 import com.example.demo.service.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +19,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -27,8 +35,10 @@ public class CustomerController {
     CustomerService customerService;
 
     @GetMapping("/load")
-    public List<Customer> getAll() {
-        return customerService.getAll();
+    public List<Customer> getAll(@RequestParam(name = "current_page", defaultValue = "0") int current_page) {
+        Pageable pageable = PageRequest.of(current_page, 5);
+        Page<Customer> page = customerService.getAll(pageable);
+        return page.getContent();
     }
 
     @GetMapping("/detail/{id}")
@@ -37,15 +47,35 @@ public class CustomerController {
     }
 
     @PostMapping("/save")
-    public Customer save(@RequestBody Customer customer) {
-        return customerService.add(customer);
+    public ResponseEntity save(@Valid @RequestBody Customer customer,
+                               BindingResult result) {
+        if (result.hasErrors()) {
+            List<String> errorMessages = new ArrayList<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errorMessages.add(error.getDefaultMessage());
+            }
+            return ResponseEntity.badRequest().body(errorMessages);
+        } else {
+            customerService.add(customer);
+            return ResponseEntity.ok("Customer added successfully");
+        }
     }
 
     @PutMapping("/update/{id}")
-    public Customer update(@RequestBody Customer customer, @PathVariable("id") String id) {
-        customer.setId(id);
-        customer.setUpdateAt(new Date());
-        return customerService.update(customer);
+    public ResponseEntity update(@Valid @RequestBody Customer customer, @PathVariable("id") String id,
+                                 BindingResult result) {
+        if (result.hasErrors()) {
+            List<String> errorMessages = new ArrayList<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errorMessages.add(error.getDefaultMessage());
+            }
+            return ResponseEntity.badRequest().body(errorMessages);
+        } else {
+            customer.setId(id);
+            customer.setUpdateAt(new Date());
+            customerService.update(customer);
+            return ResponseEntity.ok("Customer updated successfully");
+        }
     }
 
     @DeleteMapping("/delete/{id}")
