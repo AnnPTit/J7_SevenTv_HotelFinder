9b861624bf32f7c442ae67034b3573d3c282a2b7
fallinluv2003 <fallinluv2003@gmail.com>
2023-12-10 20:12:34 +0700

    'Commit'

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -365,56 +365,26 @@ public class OrderController {
         orderTimeline.setOrder(order);
         orderTimeline.setAccount(order.getAccount());
         orderTimeline.setType(Constant.ORDER_TIMELINE.WAIT_CONFIRM);
-        orderTimeline.setNote("Nhân viên tạo hóa đơn");
+        orderTimeline.setNote("Tạo hóa đơn");
         orderTimeline.setCreateAt(new Date());
         orderTimelineService.add(orderTimeline);
 
         OrderDetail orderDetail = orderDetailService.getOrderDetailById(id);
-        orderDetail.getRoom().setStatus(Constant.ROOM.EMPTY);
+        orderDetail.getRoom().setStatus(Constant.ROOM.ACTIVE);
         orderDetail.setOrder(order);
-        orderDetail.setStatus(Constant.ORDER_DETAIL.CHECKED_OUT);
+        orderDetail.setStatus(Constant.ORDER_DETAIL.CHECKED_IN);
         orderDetailService.add(orderDetail);
-
-        PaymentMethod paymentMethod = new PaymentMethod();
-        paymentMethod.setOrder(order);
-        LocalDate localDate = LocalDate.now();
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("ddMMyyyy");
-        String formatDate = localDate.format(dateTimeFormatter);
-        Random randomPayment = new Random();
-        int randomCode = randomPayment.nextInt(90000) + 10000; // Sinh số ngẫu nhiên từ 10000 đến 99999
-        String paymentMethodCode = "PT" + formatDate + randomCode;
-        paymentMethod.setPaymentMethodCode(paymentMethodCode);
-        paymentMethod.setMethod(true);
-        paymentMethod.setTotalMoney(order.getTotalMoney());
-        paymentMethod.setNote(order.getNote());
-        paymentMethod.setCreateAt(new Date());
-        paymentMethod.setCreateBy(account.getFullname());
-        paymentMethod.setUpdateAt(new Date());
-        paymentMethod.setUpdatedBy(account.getFullname());
-        paymentMethod.setStatus(Constant.COMMON_STATUS.ACTIVE);
-        paymentMethodService.add(paymentMethod);
-
-        HistoryTransaction historyTransaction = new HistoryTransaction();
-        historyTransaction.setOrder(order);
-        historyTransaction.setTotalMoney(order.getTotalMoney());
-        historyTransaction.setNote(order.getNote());
-        historyTransaction.setCreateAt(new Date());
-        historyTransaction.setCreateBy(account.getFullname());
-        historyTransaction.setUpdateAt(new Date());
-        historyTransaction.setUpdatedBy(account.getFullname());
-        historyTransaction.setStatus(Constant.COMMON_STATUS.ACTIVE);
-        historyTransactionService.add(historyTransaction);
 
         OrderTimeline timeline = new OrderTimeline();
         timeline.setOrder(order);
         timeline.setAccount(order.getAccount());
-        timeline.setType(Constant.ORDER_TIMELINE.LEAVE_EARLY);
+        timeline.setType(Constant.ORDER_TIMELINE.CHECKED_IN);
         timeline.setNote(order.getNote());
         timeline.setCreateAt(new Date());
         orderTimelineService.add(timeline);
 
         Order orderUpdate = orderService.getOrderById(order.getId());
-        orderUpdate.setStatus(Constant.ORDER_STATUS.CHECKED_OUT);
+        orderUpdate.setStatus(Constant.ORDER_STATUS.CHECKED_IN);
         orderService.add(orderUpdate);
 
         return new ResponseEntity<Order>(order, HttpStatus.OK);
