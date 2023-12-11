f4f632deecb1e0f867bf960f4ac4e734b8f521bb
quangnhph22872 <quangnhph22872@fpteduvn>
2023-12-11 21:14:26 +0700

    FixAccount

--- src/main/java/com/example/demo/service/AccountService.java
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -19,6 +19,8 @@ public interface AccountService {
 
     Boolean add(Account account);
 
+    Boolean update(Account account);
+
     Boolean delete(String id);
 
     Optional<Account> findByEmail(String email);
